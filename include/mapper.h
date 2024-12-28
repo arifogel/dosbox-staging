arifogel/dosbox-staging

commit f33cc25e57b7d1b8835f1efae7e43efa4ea7691e

Drop other bound events when binding new events in the mapper UI

src/gui/sdl_mapper.cpp
@@ -2097,6 +2097,10 @@ static void SetActiveEvent(CEvent * event) {
 		mapper.abindit=event->bindlist.begin();
 		if (mapper.abindit!=event->bindlist.end()) {
 			SetActiveBind(*(mapper.abindit));
+
+			const auto new_bind = dynamic_cast<CKeyBind*>(*mapper.abindit);
+			drop_other_bound_events(new_bind);
+
 		} else SetActiveBind(nullptr);
 		bind_but.add->Enable(true);
 	}

commit 72b72c840d2da36f3cd22e7ef4f34fb2224ec442

Enable hotplugging joysticks
* fixes #946
* add 'hotplug' configuration option in '[joystick]' section
  * controls whether game controller hotplugging is supported
  * defaults to true (enabled)
* remove REDUCE_JOYSTICK_POLLING hack, which prevented detection of joystick hotplug events
* re-initialize mapper when receiving following events:
  * SDL_CONTROLLERDEVICEADDED
  * SDL_CONTROLLERDEVICEREMOVED
  * SDL_JOYDEVICEADDED
  * SDL_JOYDEVICEREMOVED
* also fix bug preventing manual binding of joystick events introduced in
  f33cc25e57b7d1b8835f1efae7e43efa4ea7691e

include/mapper.h
@@ -83,4 +83,17 @@ void MAPPER_AutoType(std::vector<std::string> &sequence,
                      const uint32_t pacing_ms);
 void MAPPER_CheckEvent(SDL_Event *event);
 
+/**
+ * Handle a game controller being connected or disconnected by reiniitializing
+ * binds. Caller is responsible for any user interface updates, e.g. updating
+ * the mapper UI buttons to reflect joystick binds being added / removed.
+ *
+ * @param event A pointer to an SDL_JoyDeviceEvent whose type must be is one of:
+ * - SDL_CONTROLLERDEVICEADDED
+ * - SDL_CONTROLLERDEVICEREMOVED
+ * - SDL_JOYDEVICEADDED
+ * - SDL_JOYDEVICEREMOVED
+ */
+void MAPPER_HandleJoyDeviceEvent(SDL_JoyDeviceEvent* event);
+
 #endif

include/setup.h
@@ -557,4 +557,7 @@ public:
 
 bool config_file_is_valid(const std_fs::path& path);
 
+Section_prop* get_joystick_section();
+Section_prop* get_sdl_section();
+
 #endif

include/video.h
@@ -69,8 +69,6 @@
 //   postfixes is highly recommended in such cases to remove ambiguity.
 //
 
-#define REDUCE_JOYSTICK_POLLING
-
 enum class RenderingBackend {
 	Texture,
 	OpenGl
@@ -432,9 +430,7 @@ void GFX_SetMouseRawInput(const bool requested_raw_input);
 // Detects the presence of a desktop environment or window manager
 bool GFX_HaveDesktopEnvironment();
 
-#if defined(REDUCE_JOYSTICK_POLLING)
 void MAPPER_UpdateJoysticks(void);
-#endif
 
 DosBox::Rect GFX_CalcDrawRectInPixels(const DosBox::Rect& canvas_size_px,
                                       const DosBox::Rect& render_size_px,

src/dosbox.cpp
@@ -1112,6 +1112,21 @@ void DOSBOX_Init()
 	pstring->Set_help(
 	        "Apply Y-axis calibration parameters from the hotkeys ('auto' by default).");
 
+	pbool = secprop->Add_bool("hotplug", when_idle, true);
+	pbool->Set_help(
+	        "Automatically detect when a game controller is connected or disconnected\n"
+	        "(enabled by default).\n"
+	        "Currently, when this option is enabled, bindings will be reinitialized from the\n"
+	        "configured mapper file (or from defaults if it does not exist), so any unsaved\n"
+	        "binding changes made in the mapper UI will be lost whenever a controller is\n"
+	        "connected or disconnected. So you may wish to disable this option at least\n"
+	        "temporarily if you have issues losing unsaved bindings before you can save them,\n"
+	        "e.g. due to a misbehaving game controller that frequently connects /\n"
+	        "disconnects.\n"
+	        "When this option is disabled, only game controllers connected at startup will be\n"
+	        "visible to DOSBox, and any controllers that become disconnected after startup\n"
+	        "will not be available until the application is restarted.\n");
+
 	secprop = control->AddSection_prop("serial", &SERIAL_Init, changeable_at_runtime);
 	const std::vector<std::string> serials = {
 	        "dummy", "disabled", "mouse", "modem", "nullmodem", "direct"};

src/gui/sdl_mapper.cpp
@@ -749,10 +749,8 @@ public:
 
 			if (event->jaxis.which != stick_id)
 				return nullptr;
-#if defined(REDUCE_JOYSTICK_POLLING)
 			if (axis_id >= axes)
 				return nullptr;
-#endif
 			if (abs(axis_position) < 25000)
 				return nullptr;
 
@@ -764,11 +762,7 @@ public:
 		} else if (event->type == SDL_JOYBUTTONDOWN) {
 			if (event->jbutton.which != stick_id)
 				return nullptr;
-#if defined (REDUCE_JOYSTICK_POLLING)
-			return CreateButtonBind(event->jbutton.button%button_wrap);
-#else
-			return CreateButtonBind(event->jbutton.button);
-#endif
+			return CreateButtonBind(event->jbutton.button % button_wrap);
 		} else if (event->type==SDL_JOYHATMOTION) {
 			if (event->jhat.which != stick_id) return nullptr;
 			if (event->jhat.value == 0) return nullptr;
@@ -794,15 +788,18 @@ public:
 			case SDL_JOYBUTTONDOWN:
 			case SDL_JOYBUTTONUP:
 				jbutton = &event->jbutton;
-				bool state;
-				state = jbutton->type == SDL_JOYBUTTONDOWN;
-				const auto but = check_cast<uint8_t>(jbutton->button % emulated_buttons);
-				if (jbutton->which == stick_id) {
-					JOYSTICK_Button(emustick, but, state);
-				}
-				break;
-			}
-			return false;
+			        if (jbutton->which != stick_id) {
+				        break;
+			        }
+			        bool state;
+			        state = jbutton->type == SDL_JOYBUTTONDOWN;
+			        const auto but = check_cast<uint8_t>(
+			                button_wrap ? jbutton->button % emulated_buttons
+			                            : jbutton->button);
+			        JOYSTICK_Button(emustick, but, state);
+			        break;
+		        }
+		        return false;
 	}
 
 	virtual void UpdateJoystick() {
@@ -2101,7 +2098,12 @@ static void SetActiveEvent(CEvent * event) {
 			SetActiveBind(*(mapper.abindit));
 
 			const auto new_bind = dynamic_cast<CKeyBind*>(*mapper.abindit);
-			drop_other_bound_events(new_bind);
+			// TODO: version of drop_other_bound_events that works
+			// for joystick binds, for which above line assigns
+			// nullptr to newbind and causes assertion failure
+			if (new_bind) {
+				drop_other_bound_events(new_bind);
+			}
 
 		} else SetActiveBind(nullptr);
 		bind_but.add->Enable(true);
@@ -2858,17 +2860,72 @@ static bool load_binds_from_file(const std::string_view mapperfile_path,
 
 void MAPPER_CheckEvent(SDL_Event *event)
 {
-	for (auto &group : bindgroups)
-		if (group->CheckEvent(event))
+	switch (event->type) {
+	case SDL_CONTROLLERDEVICEADDED:
+	case SDL_CONTROLLERDEVICEREMOVED:
+	case SDL_JOYDEVICEREMOVED:
+	case SDL_JOYDEVICEADDED:
+		if (get_joystick_section()->Get_bool("hotplug")) {
+			MAPPER_HandleJoyDeviceEvent(
+			        reinterpret_cast<SDL_JoyDeviceEvent*>(event));
+		}
+		return;
+	default: break;
+	}
+
+	for (auto& group : bindgroups) {
+		if (group->CheckEvent(event)) {
 			return;
+		}
+	}
 }
 
-void BIND_MappingEvents() {
+void MAPPER_HandleJoyDeviceEvent(SDL_JoyDeviceEvent* event)
+{
+	SDL_Joystick* joystick;
+	switch (event->type) {
+	case SDL_CONTROLLERDEVICEADDED:
+	case SDL_JOYDEVICEADDED: {
+		auto index = event->which;
+		joystick   = SDL_JoystickOpen(index);
+		auto name  = SDL_JoystickName(joystick);
+		LOG_INFO("MAPPER: Game controller '%s' has been plugged in", name);
+		break;
+	}
+	case SDL_CONTROLLERDEVICEREMOVED:
+	case SDL_JOYDEVICEREMOVED: {
+		joystick  = SDL_JoystickFromInstanceID(event->which);
+		auto name = SDL_JoystickName(joystick);
+		LOG_INFO("MAPPER: Game controller '%s' has been disconnected", name);
+		break;
+	}
+	default:
+		LOG_ERR("MAPPER: FATAL: MAPPER_HandleJoyDeviceEvent: Unexpected event type '%d' received",
+		        event->type);
+		assert(false);
+	}
+	SDL_JoystickClose(joystick);
+	MAPPER_BindKeys(get_sdl_section());
+	mapper.redraw = true;
+}
+
+void BIND_MappingEvents()
+{
 	SDL_Event event;
 	static bool isButtonPressed = false;
 	static CButton *lastHoveredButton = nullptr;
 	while (SDL_PollEvent(&event)) {
 		switch (event.type) {
+		case SDL_CONTROLLERDEVICEADDED:
+		case SDL_CONTROLLERDEVICEREMOVED:
+		case SDL_JOYDEVICEREMOVED:
+		case SDL_JOYDEVICEADDED:
+			if (get_joystick_section()->Get_bool("hotplug")) {
+				MAPPER_HandleJoyDeviceEvent(
+				        reinterpret_cast<SDL_JoyDeviceEvent*>(&event));
+				mapper.redraw = true;
+			}
+			break;
 		case SDL_MOUSEBUTTONDOWN:
 			isButtonPressed = true;
 			/* Further check where are we pointing at right now */
@@ -3053,17 +3110,9 @@ static void CreateBindGroups() {
 		return;
 
 	if (joytype != JOY_NONE_FOUND) {
-#if defined (REDUCE_JOYSTICK_POLLING)
-		// direct access to the SDL joystick, thus removed from the event handling
-		if (mapper.sticks.num)
-			SDL_JoystickEventState(SDL_DISABLE);
-#else
 		// enable joystick event handling
 		if (mapper.sticks.num)
 			SDL_JoystickEventState(SDL_ENABLE);
-		else
-			return;
-#endif
 		// Free up our previously assigned joystick slot before assinging below
 		if (mapper.sticks.stick[mapper.sticks.num_groups]) {
 			delete mapper.sticks.stick[mapper.sticks.num_groups];
@@ -3120,15 +3169,12 @@ static void CreateBindGroups() {
 bool MAPPER_IsUsingJoysticks() {
 	return (mapper.sticks.num > 0);
 }
-
-#if defined (REDUCE_JOYSTICK_POLLING)
 void MAPPER_UpdateJoysticks() {
 	for (Bitu i=0; i<mapper.sticks.num_groups; i++) {
 		assert(mapper.sticks.stick[i]);
 		mapper.sticks.stick[i]->UpdateJoystick();
 	}
 }
-#endif
 
 void MAPPER_LosingFocus() {
 	for (const auto& event : events) {
@@ -3249,9 +3295,6 @@ void MAPPER_DisplayUI() {
 	mapper.exit = false;
 	mapper.redraw=true;
 	SetActiveEvent(nullptr);
-#if defined (REDUCE_JOYSTICK_POLLING)
-	SDL_JoystickEventState(SDL_ENABLE);
-#endif
 	while (!mapper.exit) {
 		if (mapper.redraw) {
 			mapper.redraw = false;
@@ -3283,9 +3326,6 @@ void MAPPER_DisplayUI() {
 			        SDL_GetError());
 		}
 	}
-#endif
-#if defined (REDUCE_JOYSTICK_POLLING)
-	SDL_JoystickEventState(SDL_DISABLE);
 #endif
 	GFX_ResetScreen();
 	MOUSE_NotifyTakeOver(false);
@@ -3368,8 +3408,7 @@ void MAPPER_BindKeys(Section* sec)
 	if (buttons.empty())
 		CreateLayout();
 
-	if (bindgroups.empty())
-		CreateBindGroups();
+	CreateBindGroups();
 
 	// Create binds from file or fallback to internals
 	if (!load_binds_from_file(mapper.filename, mapperfile_value))
@@ -3384,8 +3423,6 @@ void MAPPER_BindKeys(Section* sec)
 
 	if (SDL_GetModState()&KMOD_NUM)
 		MAPPER_TriggerEvent(num_lock_event, false);
-
-	GFX_RegenerateWindow(sec);
 }
 
 std::vector<std::string> MAPPER_GetEventNames(const std::string &prefix) {
@@ -3411,6 +3448,7 @@ void MAPPER_StartUp(Section* sec)
 	// mapperfile=file.map"` commands
 	constexpr auto changeable_at_runtime = true;
 	section->AddInitFunction(&MAPPER_BindKeys, changeable_at_runtime);
+	section->AddInitFunction(&GFX_RegenerateWindow, changeable_at_runtime);
 
 	// Runs one-time on shutdown
 	section->AddDestroyFunction(&MAPPER_Destroy);

src/gui/sdlmain.cpp
@@ -453,16 +453,6 @@ static double get_host_refresh_rate()
 	return refresh_rate;
 }
 
-static Section_prop* get_sdl_section()
-{
-	assert(control);
-
-	auto sdl_section = static_cast<Section_prop*>(control->GetSection("sdl"));
-	assert(sdl_section);
-
-	return sdl_section;
-}
-
 // Reset and populate the vsync settings from the config. This is
 // called on-demand after startup and on output mode changes (e.g., switching
 // from the 'texture' backend to 'opengl').
@@ -3829,15 +3819,7 @@ bool GFX_Events()
 #endif
 
 	SDL_Event event;
-#if defined (REDUCE_JOYSTICK_POLLING)
-	static auto last_check_joystick = GetTicks();
-	auto current_check_joystick = GetTicks();
-	if (GetTicksDiff(current_check_joystick, last_check_joystick) > 20) {
-		last_check_joystick = current_check_joystick;
-		if (MAPPER_IsUsingJoysticks()) SDL_JoystickUpdate();
-		MAPPER_UpdateJoysticks();
-	}
-#endif
+	MAPPER_UpdateJoysticks();
 	while (SDL_PollEvent(&event)) {
 #if C_DEBUG
 		if (is_debugger_event(event)) {
@@ -5094,6 +5076,7 @@ int sdl_main(int argc, char* argv[])
 		// All subsystems' hotkeys need to be registered at this point
 		// to ensure their hotkeys appear in the graphical mapper.
 		MAPPER_BindKeys(sdl_sec);
+		GFX_RegenerateWindow(sdl_sec);
 
 		if (arguments->startmapper) {
 			MAPPER_DisplayUI();

src/misc/setup.cpp
@@ -1980,3 +1980,23 @@ bool config_file_is_valid(const std_fs::path& path)
 	fclose(file);
 	return false;
 }
+
+static Section_prop* get_section(const char* section_name)
+{
+	assert(control);
+
+	const auto sec = static_cast<Section_prop*>(control->GetSection(section_name));
+	assert(sec);
+
+	return sec;
+}
+
+Section_prop* get_joystick_section()
+{
+	return get_section("joystick");
+}
+
+Section_prop* get_sdl_section()
+{
+	return get_section("sdl");
+}